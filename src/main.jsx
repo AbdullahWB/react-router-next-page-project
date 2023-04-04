4acec290837fc650ad55d415bf4f6b9d1019d24d
AbdullahWB <abdullah917828@gmail.com>
2023-04-04 15:47:57 +0600

    add about part

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,38 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Home from './Components/Home'
+import About from './Components/About'
+import Books from './Components/Books'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/books',
+        element: <Books></Books>
+      },
+      {
+        path: 'about',
+        element: <About />
+      }
+    ]
+  },
+  {
+    path: '/about',
+    element: <h1>hello bro</h1>
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
